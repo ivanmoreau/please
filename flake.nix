638b8eb916538e5ba986c40a7d70b581f97e9eeb
Iván Molina Rebolledo <ivanmolinarebolledo@gmail.com>
2023-01-13 17:30:45 -0600

    Basic flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = { pkgs, lib, config, system, ... }: {
+        defaultPackage = self.packages.${system}.please-build;
+      }
+    }
+}
