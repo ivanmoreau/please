ef3a4300bb66226625034d34777f3c0c03a69f07
Iván Molina Rebolledo <ivanmolinarebolledo@gmail.com>
2023-01-14 01:17:36 -0600

    Update flake.nix
    Undocumented stuff.

--- flake.nix
+++ flake.nix
@@ -14,27 +14,18 @@
           depsBuildBuild = with pkgs; [ go ];
           buildPhase = ''
             export HOME=$(pwd)
-            echo $HOME
             go run -race src/please.go $PLZ_ARGS --log_file plz-out/log/bootstrap_build.log build //src:please
-            ls $HOME
+            plz-out/bin/src/please build //package:installed_files
           '';
           installPhase = ''
             mkdir -p $out
-            ls plz-out
-            ls plz-out/bin
-            ls plz-out/bin/package/
             OUTPUTS="`plz-out/bin/src/please query outputs //package:installed_files`"
             for OUTPUT in $OUTPUTS; do
               TARGET="$out/$(basename $OUTPUT)"
               rm -f "$TARGET"  # Important so we don't write through symlinks.
-              echo AAA
-              echo $OUTPUT
-              echo $TARGET
-              echo bbb
               cp $OUTPUT $TARGET
               chmod 0775 $TARGET
             done
-            ln -sf "$out/please" "$out/plz"
             chmod 0664 "$out/junit_runner.jar"
             mkdir -p $out/bin
             ln -s $out/please $out/bin/please
